1be06e16464342843e2a9bb4fb43153a36bb3041
Anup <anupthedev@gmail.com>
2023-10-18 15:46:06 -0400

    Added enroll button in the accordion

--- src/components/Courses/CourseItem/CourseItem.js
+++ src/components/Courses/CourseItem/CourseItem.js
@@ -59,14 +59,11 @@ export const CourseItem = ({ name }) => {
 
 			{isOpen && (
 				<div className='accordion-content mt-2 bg-opacity-50 bg-white border border-midnight rounded-md px-5 py-10 text-lg'>
-					<div className='syllabus'>
-						<h2 className='font-bold text-2xl mb-7'>Syllabus</h2>
-						{/* <p className='text-gray-600 mb-5'>
-							Our courses come with interactive lessons, real-world projects.
-							Take your first step towards mastering a new skill or advancing in
-							your career. Browse through our extensive catalog and find the
-							course that's just right for you.
-						</p> */}
+					<div className='syllabus flex justify-between items-center mb-7'>
+						<h2 className='font-bold text-2xl'>Syllabus</h2>
+						<button className='enroll-button bg-our-yellow hover:bg-[#e6b00f] transition duration-300 ease-in-out font-bold rounded-md px-5 py-3'>
+							Enroll
+						</button>
 					</div>
 
 					<ol className='list-decimal list-inside space-y-7'>
