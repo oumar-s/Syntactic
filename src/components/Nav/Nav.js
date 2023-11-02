86f65900d7f6ce8d2dad9ffcd0eccdef4ee6de17
Anup <anupthedev@gmail.com>
2023-11-01 20:54:28 -0400

    UI tweaks, bug fix, etc

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -37,7 +37,6 @@ export const Nav = () => {
 			if (user) {
 				setIsUserLoggedIn(true);
 				setUser(user);
-				console.log('displayName from Nav:', user.displayName);
 				setIsDropdownVisible(false); // Reset the dropdown visibility
 			} else {
 				setIsUserLoggedIn(false);
@@ -53,7 +52,6 @@ export const Nav = () => {
 
 	const logout = async () => {
 		try {
-
 			await signOut(auth);
 			setIsDropdownVisible(false); // Reset the dropdown visibility
 		} catch (err) {
@@ -62,7 +60,7 @@ export const Nav = () => {
 	};
 
 	return (
-		<nav className='font-inconsolata text-lg bg-white border-b border-midnight w-full p-6'>
+		<nav className='font-inconsolata text-lg bg-black text-white w-full p-6'>
 			<div
 				className='container mx-auto flex items-center justify-between relative'
 				style={{ maxWidth: '1800px' }}
@@ -144,12 +142,9 @@ export const Nav = () => {
 					>
 						{isUserLoggedIn ? (
 							<>
-								<div
-									
-									className='user-icon cursor-pointer rounded-ful w-12 h-12 flex justify-center items-center text-white font-bold'
-								>
+								<div className='user-icon cursor-pointer rounded-full w-12 h-12 p-1 flex justify-center items-center border-2 border-midnight text-white font-bold'>
 									<img
-										src={user? user.photoURL : menu}
+										src={user ? user.photoURL : menu}
 										alt='avatar'
 										className='w-full h-full object-cover rounded-full'
 										ref={userIconRef} // Attach the ref here
@@ -157,7 +152,7 @@ export const Nav = () => {
 									/>
 								</div>
 								{isDropdownVisible && (
-									<div className='dropdown-menu absolute w-36 top-20 right-0 bg-white rounded-md shadow-md border border-midnight p-5'>
+									<div className='dropdown-menu absolute w-36 top-20 right-0 bg-black rounded-md shadow-md border-2 border-midnight p-5'>
 										<ul className='dropdown-ul'>
 											<li className='dropdown-li mb-4 transition duration-200 hover:text-neon-blue'>
 												<NavLink
@@ -168,7 +163,7 @@ export const Nav = () => {
 													Profile
 												</NavLink>
 											</li>
-											<hr className='my-3' />
+											{/* <hr className='my-3' /> */}
 											<li className='dropdown-li transition duration-200 hover:text-neon-blue'>
 												<NavLink to='/login' onClick={logout}>
 													Log Out
